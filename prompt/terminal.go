7e05e277815f9e97ea656e94e1c85793cb0251fe
Evan Bovie <evan@bovie.me>
2022-09-02 22:12:03 -0700

    Configure prompt/terminal to prefer `/dev/tty` over `os.Stdin`
    Aims to add support for `credential_process` and MFA as mentioned in #1015 and @gdamjan's suggestion in https://github.com/99designs/aws-vault/issues/405#issuecomment-549071583

--- prompt/terminal.go
+++ prompt/terminal.go
@@ -10,28 +10,32 @@ import (
 )
 
 func TerminalPrompt(message string) (string, error) {
-	fmt.Fprint(os.Stderr, message)
+	return withTerminal(func(in, out *os.File) (string, error) {
+		fmt.Fprint(out, message)
 
-	reader := bufio.NewReader(os.Stdin)
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
+		reader := bufio.NewReader(in)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
 
-	return strings.TrimSpace(text), nil
+		return strings.TrimSpace(text), nil
+	})
 }
 
 func TerminalSecretPrompt(message string) (string, error) {
-	fmt.Fprint(os.Stderr, message)
+	return withTerminal(func(in, out *os.File) (string, error) {
+		fmt.Fprint(out, message)
 
-	text, err := term.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil {
-		return "", err
+		text, err := term.ReadPassword(int(in.Fd()))
+		if err != nil {
+			return "", err
+		}
+
+		fmt.Println()
+
+		return strings.TrimSpace(string(text)), nil
 	}
-
-	fmt.Println()
-
-	return strings.TrimSpace(string(text)), nil
 }
 
 func TerminalMfaPrompt(mfaSerial string) (string, error) {
@@ -40,4 +44,19 @@ func TerminalMfaPrompt(mfaSerial string) (string, error) {
 
 func init() {
 	Methods["terminal"] = TerminalMfaPrompt
+}
+
+// withTerminal runs f with the terminal input and output files, if available.
+// withTerminal does not open a non-terminal stdin, so the caller does not need
+// to check stdinInUse.
+// Copyright 2021 The age Authors
+// This method is governed by a BSD-style license found at
+// https://github.com/FiloSottile/age/blob/main/LICENSE
+func withTerminal(f func(in, out *os.File) (string, error)) (string, error) {
+	if tty, err := os.OpenFile("/dev/tty", os.O_RDWR, 0); err == nil {
+		defer tty.Close()
+		return f(tty, tty)
+	} else
+		return f(os.Stdin, os.Stderr)
+	}
 }
